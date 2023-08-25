4631175ee872965b6f1b2d88d87a5ebe2f8076cb
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:51:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1POO7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QualMaior a;
+            a = new QualMaior();
+
+            Console.WriteLine("valor de n1");
+            a.setn1(double.Parse(Console.ReadLine()));
+            Console.WriteLine("valor de n2");
+            a.setn2(double.Parse(Console.ReadLine()));
+
+            a.calcular();
+            Console.WriteLine("situação", a.getn1(), a.getn2());
+        }
+    }
+}
